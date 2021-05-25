e9784edda9ec8cd17dc76c5bb677487db4015149
F3licia <romeofelicia@gmail.com>
2021-05-25 16:30:04 +0200

    compilati data js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ const app = new Vue({
     el: '#app',
     data:{
         logo:["images/logo.png"],
-        hotTopic:[],
+        firstRowHotTopic:[],
+        secRowHotTopic:[],
+       
         allPages:[
             {no:0, title:"Home", href:"#"},
             {no:1, title:"Apple", href:"#"},
@@ -11,60 +13,63 @@ const app = new Vue({
             {no:0, title:"Forums", href:"#"},
             {no:0, title:"Contact us", href:"#"},
         ],
+
+        inEvidence:{
+            title:"Questo è il titolo di un articolo in evidenza",
+            commts:"15",
+            href:"#",
+            img:"./images/featured_article_1_bg.jpg",
+            artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
+        },
+
         articles:[
             {
-                title:"Questo è il titolo di un articolo n1",
-                commts:"4",
+                title:"Questo è il titolo di un articolo",
+                commts:"20",
                 href:"#",
-                featured: false,
                 img:"./images/post_feat_img_22-700x441.jpg",
                 artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
             },
             {
-                title:"Questo è il titolo di un articolo n.2 ma in evidenza",
+                title:"Questo è il titolo di un articolo ",
                 commts:"15",
                 href:"#",
-                featured: true,
-                img:"./images/featured_article_1_bg.jpg",
+                img:"./images/post_feat_img_22-700x441.jpg",
                 artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
             },
             {
-                title:"Questo è il titolo di un articolo n3",
-                commts:"4",
+                title:"Questo è il titolo di un articolo",
+                commts:"21",
                 href:"#",
-                featured: false,
                 img:"./images/post_feat_img_21-700x441.jpg",
                 artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
             },
             {
-                title:"Questo è il titolo di un articolo n4",
-                commts:"4",
+                title:"Questo è il titolo di un articolo",
+                commts:"15",
+                href:"#",
+                img:"./images/post_feat_img_20-700x441.jpg",
+                artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
+            },
+            {
+                title:"Questo è il titolo di un articolo",
+                commts:"19",
                 href:"#",
                 featured: false,
                 img:"./images/post_feat_img_20-700x441.jpg",
                 artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
             },
             {
-                title:"Questo è il titolo di un articolo n.5 ma in evidenza",
-                commts:"11",
+                title:"Questo è il titolo di un articolo",
+                commts:"18",
                 href:"#",
-                featured: false,
                 img:"./images/post_feat_img_20-700x441.jpg",
                 artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
             },
             {
-                title:"Questo è il titolo di un articolo n6",
-                commts:"11",
-                href:"#",
-                featured: false,
-                img:"./images/post_feat_img_20-700x441.jpg",
-                artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
-            },
-            {
-                title:"Questo è il titolo di un articolo n7",
+                title:"Questo è il titolo di un articolo",
                 commts:"10",
                 href:"#",
-                featured: false,
                 img:"./images/post_feat_img_20-700x441.jpg",
                 artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
             },
@@ -96,15 +101,30 @@ const app = new Vue({
         ]
     },
     methods: {              //ritorna tre articoli con più di 10 commenti 
-        filteredHotTopic() {  
-            this.hotTopic= this.articles.filter((artc) => {           
-                return (artc.commts >= 10) * 3
+        firstHotTopic() {  
+            this.firstRowHotTopic= this.articles.filter((artc) => {           
+                return (artc.commts >= 10) 
             });
-            return hotTopic;
+            return firstRowHotTopic;
         },
+        secondHotTopic() {  
+            this.secRowhotTopic= this.articles.filter((artc) => {     
+                    return (artc.commts >= 10)
+               
+            });
+            return secRowhotTopic;
+        },
+        getFeautered() {  
+
+        },
+
     },
     mounted()  {
-         this.filteredHotTopic()
+         this.firstHotTopic();
+         this.secondHotTopic();
+
     }, 
+    
         
 }) 
+

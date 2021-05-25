3941d4ebbaed3039c07840964fccd7e2bad59347
F3licia <romeofelicia@gmail.com>
2021-05-25 02:06:51 +0200

    aggiunti articoli in data e layout

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,37 @@ const app = new Vue({
             {no:0, title:"Forums", href:"#"},
             {no:0, title:"Contact us", href:"#"},
         ],
+        articles:[
+            {
+                title:"Questo è il titolo di un articolo n1",
+                href:"#",
+                featured: false,
+                img:"/images/post_feat_img_22-700x441.jpg",
+                artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
+            },
+            {
+                title:"Questo è il titolo di un articolo n.2 ma in evidenza",
+                href:"#",
+                featured: true,
+                img:"/images/featured_article_1_bg.jpg",
+                artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
+            },
+            {
+                title:"Questo è il titolo di un articolo n3",
+                href:"#",
+                featured: false,
+                img:"/images/post_feat_img_21-700x441.jpg",
+                artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
+            },
+            {
+                title:"Questo è il titolo di un articolo n4",
+                href:"#",
+                featured: false,
+                img:"/images/post_feat_img_20-700x441.jpg",
+                artc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum culpa itaque saepe unde asperiores atque nobis temporibus facere minus voluptate, labore odio fugiat, veniam, sint placeat alias tempora quae.",
+            },
+         
+        ],
         popularTopics:[
             {no:0, title:"", href:"#"},
             {no:1, title:"", href:"#"},
